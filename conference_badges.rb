adb506734dc27175111853206dfe1c4bc13601e6
Jeffrey Yu <jwy5140@gmail.com>
2019-02-20 07:00:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,5 +1,5 @@
 def badge_maker (name)
-  "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator (arr)
@@ -7,5 +7,5 @@ def batch_badge_creator (arr)
   arr.each do |n|
     messages.push(badge_maker(n))
   end
-  messages
+  return messages
 end
